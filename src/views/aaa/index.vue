5df2313a450b1fd1ce0736017c707df9c7e9b7fd
LiXinyue <1626854179@qq.com>
2021-04-09 21:05:39 +0800

    test

---
+++ src/views/aaa/index.vue
@@ -0,0 +1,45 @@
+<!--  -->
+<template>
+    <div class=''>aaaaaaa</div>
+</template>
+
+<script>
+    
+export default {
+    
+    components: {},
+    data() {
+        
+        return {
+            
+        };
+    },
+    //监听属性 类似于data概念
+    computed: {},
+    //监控data中的数据变化
+    watch: {},
+    
+    methods: {
+        
+    },
+    //生命周期 - 创建完成（可以访问当前this实例）
+    created() {
+        
+    },
+    //生命周期 - 挂载完成（可以访问DOM元素）
+    mounted() {
+        
+    },
+    beforeCreate() {}, 
+    beforeMount() {},
+    beforeUpdate() {}, 
+    updated() {}, 
+    beforeDestroy() {}, 
+    destroyed() {}, 
+    //如果页面有keep-alive缓存功能，这个函数会触发
+    activated() {}, 
+    }
+</script>
+<style  scoped>
+    
+</style>
